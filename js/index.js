9b009d73b031f9595c4735e02cc9c431b652b33c
Littl3S3al <lauseal@gmail.com>
2021-08-11 11:19:26 +0200

    fixing more broken links

--- js/index.js
+++ js/index.js
@@ -72,13 +72,13 @@ const articles_link = document.querySelector('#dash-link-2'); 
 const PDF_link = document.querySelector('#dash-link-3'); 
 
 fullData_link.addEventListener('click', e => {
-    document.location.href = '../pages/full-data.html'
+    document.location.href = 'full-data.html'
 })
 articles_link.addEventListener('click', e => {
-    document.location.href = '../pages/articles.html'
+    document.location.href = 'articles.html'
 })
 PDF_link.addEventListener('click', e => {
-    document.location.href = '../pages/reports.html'
+    document.location.href = 'reports.html'
 })
 
 
